bc8a8686b9dd68752e3846beb511f7faf5b9d9ff
Daniel064e <dal0010@alu.medac.es>
2025-11-10 19:24:47 +0100

    Commit practica5 ejercicio 2.3

---
+++ app/src/main/java/com/example/progrmacinmultimedia5ejercicio2/Juegos.java
@@ -0,0 +1,15 @@
+package com.example.progrmacinmultimedia5ejercicio2;
+
+public class Juegos {
+
+    private String title;
+
+    private String image;
+
+    public Juegos(String title, String image) {
+
+        this.title = title;
+
+        this.image = image;
+    }
+}
